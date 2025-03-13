96547c327254d9e8be39c33f322aa8d2f0f8cdd7
sajjadali56 <sajjad.ali@badriconsultancy.com>
2025-03-13 13:48:13 +0500

    Changed the menu bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,34 +1,31 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "react-bootstrap/Image";
-// import Image from "next/image";
 import Link from "next/link";
-
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-
-import { routes, navLinks } from "@data/routes";
 import { useDispatch, useSelector } from "react-redux";
 import { selectUser, setUser } from "./redux/slices/admin";
+
+import { routes, navLinks } from "@data/routes";
+import { Navbar, Nav, Container, Offcanvas, Button } from "react-bootstrap";
 
 const NavBar = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
 
-  const [links, setLinks] = React.useState(navLinks);
+  const [links, setLinks] = useState(navLinks);
+  const [showDrawer, setShowDrawer] = useState(false);
 
   useEffect(() => {
     if (!user) {
-      setLinks([...links]);
+      setLinks([...navLinks]);
     } else {
       const unwantedRoutes = [
         routes.contact,
         routes.education,
         routes.experience,
       ];
-      const updatedLinks = links.filter(
+      const updatedLinks = navLinks.filter(
         (link) => !unwantedRoutes.includes(link.href)
       );
       setLinks([
@@ -39,7 +36,6 @@ const NavBar = () => {
           label: "Logout",
           onClick: () => {
             dispatch(setUser(""));
-
             window.location.reload();
           },
         },
@@ -47,18 +43,27 @@ const NavBar = () => {
     }
   }, [user]);
 
-  console.log(user);
   return (
     <Navbar expand="lg" className="bg-light shadow-sm">
       <Container>
+        {/* Logo */}
         <Link href={routes.home} passHref legacyBehavior>
           <Navbar.Brand className="d-flex align-items-center">
             <Image src="/logo.png" alt="Profile" width={50} height={50} />
           </Navbar.Brand>
         </Link>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        {/* Mobile Menu Button */}
+        <Button
+          variant="outline-dark"
+          className="d-lg-none"
+          onClick={() => setShowDrawer(true)}
+        >
+          ☰
+        </Button>
+
+        {/* Desktop Navigation */}
+        <Navbar.Collapse id="basic-navbar-nav" className="d-none d-lg-flex">
           <Nav className="ms-auto">
             {links.map(({ href, label, onClick }) => (
               <Link key={`${href}${label}`} href={href} passHref legacyBehavior>
@@ -68,6 +73,33 @@ const NavBar = () => {
           </Nav>
         </Navbar.Collapse>
       </Container>
+
+      {/* Offcanvas Drawer for Mobile */}
+      <Offcanvas
+        show={showDrawer}
+        onHide={() => setShowDrawer(false)}
+        placement="end"
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Navigation</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="flex-column">
+            {links.map(({ href, label, onClick }) => (
+              <Link key={`${href}${label}`} href={href} passHref legacyBehavior>
+                <Nav.Link
+                  onClick={() => {
+                    setShowDrawer(false);
+                    if (onClick) onClick();
+                  }}
+                >
+                  {label}
+                </Nav.Link>
+              </Link>
+            ))}
+          </Nav>
+        </Offcanvas.Body>
+      </Offcanvas>
     </Navbar>
   );
 };
