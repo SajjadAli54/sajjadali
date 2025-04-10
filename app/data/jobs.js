5c772d89fc827ff35b23319833aaac8d18296323
sajjadali56 <sajjad.ali@badriconsultancy.com>
2025-04-10 11:38:37 +0500

    Resolved the image url issue in jobs

--- app/data/jobs.js
+++ app/data/jobs.js
@@ -1,8 +1,7 @@
 export const jobs = [
   {
     title: "Software Developer",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoaIHsfXc6tbhxhb7Nx8cu2_ojVFJG-LM5bw&s",
+    image: "/badri.jpg",
     companyName: "Badri Management Consultancy",
     companyUrl: "https://badriconsultancy.com/",
     startDate: "Nov 2024",
@@ -27,41 +26,16 @@ export const jobs = [
     title: "MERN Stack Developer",
     companyName: "Solspro",
     companyUrl: "https://sols-pro.com/",
-    startDate: "July 2024",
+    startDate: "April 2024",
     endDate: "Oct 2024",
     subtitle:
       "Led the development of dynamic web and mobile applications with a focus on user engagement and scalability.",
     duties: [
       "🌐 Integrated Web3 wallets (MetaMask and Solana Phantom) into React.js applications, boosting customer acquisition by 30%.",
       "🔐 Developed and deployed secure REST APIs, ensuring robust backend services for multiple applications.",
-      "📱 Enhanced shopping app traffic by 15% through the implementation of deep links and QR code features using Flutter.",
+      "📱 Enhanced shopping app traffic by 15% through the implementation of deep links and QR code features inside the Flutter App.",
     ],
     tags: ["MERN", "Flutter", "Web3", "REST APIs", "Django"],
-    image:
-      "https://media.licdn.com/dms/image/v2/C4D0BAQF_YNRP5KKfDQ/company-logo_200_200/company-logo_200_200/0/1675170573364?e=1744243200&v=beta&t=Lri-9-iIjup97IyToN1Ls5WPMP00oJ6wpODByg1JlAI",
-  },
-  {
-    title: "Machine Learning Engineer (Intern)",
-    subtitle: "Driving innovation with cutting-edge ML models and techniques.",
-    companyName: "Syzen Technologies",
-    companyUrl:
-      "https://www.linkedin.com/company/syzen-technologies/posts/?feedView=all",
-    startDate: "Aug 2023",
-    endDate: "Oct 2023",
-    duties: [
-      "🤖 Developed and fine-tuned state-of-the-art ML models, leveraging advanced techniques such as diffusion models and transformers.",
-      "🌐 Collaborated with web development teams to integrate ML models into backend systems, ensuring seamless deployment.",
-      "🔧 Built comprehensive frontend and backend services to deploy machine learning models in production environments.",
-    ],
-    tags: [
-      "Python",
-      "Machine Learning",
-      "PyTorch",
-      "TensorFlow",
-      "Self-Supervision",
-      "OpenCV",
-    ],
-    image:
-      "https://media.licdn.com/dms/image/v2/D4D0BAQHcR7CIdzc9Fw/company-logo_200_200/company-logo_200_200/0/1710930691786?e=1744243200&v=beta&t=ldiy-Ws7P-zt6E296PM-C_TzuEf8_kpdEcvzwHnxkI0",
+    image: "/solspro.png",
   },
 ];
