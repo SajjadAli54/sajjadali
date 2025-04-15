875d9cacec1196749325907ead15743e37a71f36
sajjadali56 <sajjad.ali@badriconsultancy.com>
2025-04-15 16:12:37 +0500

    Updated stack

--- app/components/stack/TechStack.tsx
+++ app/components/stack/TechStack.tsx
@@ -29,10 +29,11 @@ function TechStack({ techItems }: Props) {
   const MOBILE_PAGE_SIZE = 2;
   const DESKTOP_PAGE_SIZE = 6;
   const CURRENT_PAGE = 1;
+  
 
   const [currentPage, setCurrentPage] = useState(CURRENT_PAGE);
   const [pageSize, setPageSize] = useState(
-    typeof window !== "undefined" && window.innerWidth < MOBILE_WIDTH
+     window.innerWidth < MOBILE_WIDTH
       ? MOBILE_PAGE_SIZE
       : DESKTOP_PAGE_SIZE
   );
