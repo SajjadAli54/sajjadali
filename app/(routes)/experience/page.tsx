43a469411fddf4544c65dabc37feec3bfed259a1
sajjadali56 <sajjad.ali@badriconsultancy.com>
2025-05-03 22:12:43 +0500

    Changed design for jobs section

--- app/(routes)/experience/page.tsx
+++ app/(routes)/experience/page.tsx
@@ -1,29 +1,45 @@
+"use client";
 import ExperienceCard from "@components/ExperienceCard";
-
 import { jobs } from "@data/jobs";
+import styles from "./Experience.module.css"; // You'll create this CSS module
+import MyCard from "@/app/components/Card";
+import { FaBuilding } from "react-icons/fa";
+import { Fa42Group } from "react-icons/fa6";
 
 function Experience() {
   return (
-    <div className="container ">
-      <h3 className="text-center mb-5">
-        Here are some of the exciting roles I have had, where I have grown my
-        skills and contributed to impactful projects.
-      </h3>
-      <div className="d-flex flex-column align-items-stretch gap-4">
+    <div className={`container ${styles.timelineContainer}`}>
+      <div className={styles.timeline}>
         {jobs.map((job, index) => (
-          <ExperienceCard
+          <div
             key={index}
-            type="job"
-            title={job.title}
-            image={job.image}
-            institutionOrCompany={job.companyName}
-            institutionOrCompanyUrl={job.companyUrl}
-            startDate={job.startDate}
-            endDate={job.endDate}
-            subtitle={job.subtitle}
-            achievementsOrDuties={job.duties}
-            tags={job.tags}
-          />
+            className={`${styles.timelineItem} ${
+              index % 2 === 0 ? styles.left : styles.right
+            }`}
+          >
+            <MyCard
+              image={job.image}
+              title={job.title}
+              description={job.subtitle}
+              tags={job.tags}
+              links={[
+                { url: job.companyUrl, label: FaBuilding },
+                { url: `/experience/${index}`, label: Fa42Group },
+              ]}
+            />
+            {/* <ExperienceCard
+              type="job"
+              title={job.title}
+              image={job.image}
+              institutionOrCompany={job.companyName}
+              institutionOrCompanyUrl={job.companyUrl}
+              startDate={job.startDate}
+              endDate={job.endDate}
+              subtitle={job.subtitle}
+              achievementsOrDuties={job.duties}
+              tags={job.tags}
+            /> */}
+          </div>
         ))}
       </div>
     </div>
